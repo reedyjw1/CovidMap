33aeff5959807671210b8d1f17a39aa69d8d4a9b
John Reedy <johnreedy@johns-mbp-2.attlocal.net>
2020-12-18 17:51:46 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function getCovidByState(callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if(httpRequest.readyState == 4 && httpRequest.status == 200)
+        callback(httpRequest.responseText)
+    }
+    httpRequest.open("GET", "https://api.covidtracking.com/v1/states/current.json", true);
+    httpRequest.send(null)
+}
+
+function displayData(text){
+    var obj = JSON.parse(text)
+
+    for(var i = 0; i < obj.length; i++){
+        console.log(obj[i].state)
+    }
+}
