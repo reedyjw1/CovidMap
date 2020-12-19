9bb5f1e60ae3bb284713ac415ce048729528abd1
John Reedy <johnreedy@johns-mbp-2.attlocal.net>
2020-12-18 22:46:15 -0500

    used mapbox geojson

--- mapboxStyle.js
+++ mapboxStyle.js
@@ -19,23 +19,80 @@ function readTextFile(map)
 }
 
 function addData(featureArray, map){
-    const introGeoJson = "{\"type\": \"geojson\",\"data\": {\"type\": \"Feature\", \"geometry\": "
+    // const introGeoJson = "{\"type\": \"geojson\",\"data\": {\"type\": \"Feature\", \"geometry\": "
 
-    for(var i = 0; i < featureArray.length; i++){
-        var concatString = introGeoJson + JSON.stringify(featureArray[i].geometry) + "}}"
+    // for(var i = 0; i < featureArray.length; i++){
+    //     var concatString = introGeoJson + JSON.stringify(featureArray[i].geometry) + "}}"
+        // map.addLayer({
+        //     'id': featureArray[i].properties.NAME,
+        //     'type': 'fill',
+        //     'source': featureArray[i].properties.NAME,
+        //     'layout': {},
+        //     'paint': {
+        //         'fill-color': '#088',
+        //         'fill-opacity': [
+        //             'case',
+        //             ['boolean', ['feature-state', 'hover'], false],
+        //             .5,
+        //             0.1
+        //             ]
+        //     }
+        //     });
+    //     map.addSource(featureArray[i].properties.NAME, JSON.parse(concatString))
+    // }
+    var hoveredStateId = null;
 
-        map.addSource(featureArray[i].properties.NAME, JSON.parse(concatString))
-        map.addLayer({
-            'id': featureArray[i].properties.NAME,
-            'type': 'fill',
-            'source': featureArray[i].properties.NAME,
-            'layout': {},
-            'paint': {
-                'fill-color': '#088',
-                'fill-opacity': 0.1
+    map.addSource('states', {
+        'type': 'geojson',
+        'data':
+        'https://docs.mapbox.com/mapbox-gl-js/assets/us_states.geojson'
+        });
+
+    map.addLayer({
+        'id': 'states-fill',
+        'type': 'fill',
+        'source': 'states',
+        'layout': {},
+        'paint': {
+            'fill-color': '#088',
+            'fill-opacity': [
+                'case',
+                ['boolean', ['feature-state', 'hover'], false],
+                .4,
+                0.1
+                ]
+        }
+    });
+
+    map.on('mousemove', 'states-fill', function (e) {
+        if (e.features.length > 0) {
+            if (hoveredStateId) {
+                map.setFeatureState(
+                    { source: 'states', id: hoveredStateId },
+                    { hover: false }
+                );
             }
-            });
-    }
+            hoveredStateId = e.features[0].id;
+            map.setFeatureState(
+                { source: 'states', id: hoveredStateId },
+                { hover: true }
+            );
+        }
+    });
+         
+    // When the mouse leaves the state-fill layer, update the feature state of the
+    // previously hovered feature.
+    map.on('mouseleave', 'states-fill', function () {
+        if (hoveredStateId) {
+            map.setFeatureState(
+                { source: 'states', id: hoveredStateId },
+                { hover: false }
+            );
+        }
+        hoveredStateId = null;
+    });
+
+
 }
 
 function getStateMapKey() {
