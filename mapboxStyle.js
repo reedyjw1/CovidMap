7033df0f956e9a7b5e7d194acfa58db3f4ece1c4
John Reedy <johnreedy@johns-mbp-2.attlocal.net>
2020-12-18 22:20:34 -0500

    reversed json map of state codes

--- mapboxStyle.js
+++ mapboxStyle.js
@@ -23,7 +23,7 @@ function addData(featureArray, map){
 
     for(var i = 0; i < featureArray.length; i++){
         var concatString = introGeoJson + JSON.stringify(featureArray[i].geometry) + "}}"
-        
+
         map.addSource(featureArray[i].properties.NAME, JSON.parse(concatString))
         map.addLayer({
             'id': featureArray[i].properties.NAME,
@@ -36,4 +36,22 @@ function addData(featureArray, map){
             }
             });
     }
+}
+
+function getStateMapKey() {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if(httpRequest.readyState == 4 && httpRequest.status == 200){
+            var statesObj = JSON.parse(httpRequest.responseText);
+            let stateCodeToStates = new Object();
+
+            for (let el in statesObj){
+                stateCodeToStates[statesObj[el]] = el
+            }
+            console.log(stateCodeToStates)
+        }
+    }
+    httpRequest.open("GET", "https://gist.githubusercontent.com/mshafrir/2646763/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_hash.json", true);
+
+    httpRequest.send(null)
 }
