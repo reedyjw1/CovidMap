f60915c1bc69e6eae6101e15d6c615d47c81b2a3
John Reedy <johnreedy@johns-mbp-2.attlocal.net>
2020-12-23 17:02:52 -0500

    cached api response

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
-function getCovidByState(codeMap, name) {
+function getAPI() {
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function() {
         if(httpRequest.readyState == 4 && httpRequest.status == 200){
-            var jsonResponse = JSON.parse(httpRequest.responseText);
-            let formattedResponse = new Object();
-            for (let el in jsonResponse){
-                formattedResponse[codeMap[jsonResponse[el].state]] = jsonResponse[el];
-            }
-            alert("Deaths: " + formattedResponse[name].death)
-            //console.log(formattedResponse);
+            sessionStorage.setItem('covidData', httpRequest.responseText);
+            console.log("request made.")
+
         }
     }
     httpRequest.open("GET", "https://api.covidtracking.com/v1/states/current.json", true);
     httpRequest.send(null)
 }
 
-function getStateMapKey(callback, name) {
+function getStateDictionary(){
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if(httpRequest.readyState == 4 && httpRequest.status == 200){
+            sessionStorage.setItem('stateDictionary', httpRequest.responseText);
+        }
+    }
+    httpRequest.open("GET", "https://gist.githubusercontent.com/mshafrir/2646763/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_hash.json", true);
+
+    httpRequest.send(null)
+}
+
+function getCovidByState(name, map, e){
+    var statesObj = JSON.parse(sessionStorage.getItem('stateDictionary'));
+    var covidObject = JSON.parse(sessionStorage.getItem('covidData'));
+
+    if(statesObj !== null && covidObject !== null){
+        var formattedResponse = new Object()
+        for (let el in covidObject){
+            formattedResponse[statesObj[covidObject[el].state]] = covidObject[el];;
+        }
+
+        alert(formattedResponse[name].death);
+    }
+    
+}
+
+
+function getStateMapKey(name) {
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function() {
         if(httpRequest.readyState == 4 && httpRequest.status == 200){
@@ -28,7 +52,14 @@ function getStateMapKey(callback, name) {
             //     stateCodeToStates[statesObj[el]] = el;
             // }
             //console.log(stateCodeToStates);
-            callback(statesObj, name);
+            var jsonResponse = JSON.parse(sessionStorage.getItem('covidData'))
+            var formattedResponse = new Object()
+            for (let el in jsonResponse){
+                formattedResponse[statesObj[jsonResponse[el].state]] = jsonResponse[el];
+                sessionStorage.setItem('covidData', JSON.stringify(formattedResponse));
+            }
+
+            alert(formattedResponse[name].death);
         }
     }
     httpRequest.open("GET", "https://gist.githubusercontent.com/mshafrir/2646763/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_hash.json", true);
